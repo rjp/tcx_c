06f31617f6832f1b8f9c9d3648e25c700394979d
rjp <rjpartington@googlemail.com>
2013-01-16 22:04:57 +0000

    Parse laps and sum distance

--- test-read.c
+++ test-read.c
@@ -1,10 +1,26 @@
 #include <mxml.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+double lap_distance(mxml_node_t *lap)
+{
+    mxml_node_t *distance;
+
+    distance = mxmlFindElement(lap, lap, "DistanceMeters", NULL, NULL, MXML_DESCEND_FIRST);
+    if (distance) {
+        const char *s_dist;
+        s_dist = mxmlGetText(distance, NULL);
+        return atof(s_dist);
+    }
+
+    return (double)0.0;
+}
 
 void find_laps(mxml_node_t *tree)
 {
     mxml_node_t *actone, *lap;
     int lap_count = 0;
+    double total_dist = 0;
 
     actone = mxmlFindElement(tree, tree, "Activity", NULL, NULL, MXML_DESCEND);
 
@@ -13,19 +29,23 @@ void find_laps(mxml_node_t *tree)
         lap != NULL;
         lap = mxmlFindElement(lap, tree, "Lap", NULL, NULL, MXML_NO_DESCEND)
     )  {
+        float l_dist;
         const char *lst;
         lap_count++;
         lst = mxmlElementGetAttr(lap, "StartTime");
-        printf("Lap %d starts at %s\n", lap_count, lst);
+        l_dist = lap_distance(lap);
+        printf("Lap %d starts at %s, %.2f metres\n", lap_count, lst, l_dist);
+        total_dist = total_dist + l_dist;
     }
+    printf("Total dist: %.2f metres\n", total_dist);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
     FILE *fp;
     mxml_node_t *tree;
 
-    fp = fopen("/Users/rjp/dunwich.tcx", "r");
+    fp = fopen(argv[1], "r");
     tree = mxmlLoadFile(NULL, fp, MXML_TEXT_CALLBACK);
     fclose(fp);
 
